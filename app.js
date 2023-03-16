4777270739254c5d45120a1d6b8c480ffae50a12
Koray Dundar <koray.dundar@exove.com>
2023-03-16 16:28:23 +0200

    Basic server setup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+    res.send('Hello world!')
+})
+
+const PORT = 3000
+
+app.listen(PORT, console.log(`Server is listening ${PORT}`))
