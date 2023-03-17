96cbb81e17d042eee3e02eb5efe959e033a86ea8
Koray Dundar <koray.dundar@exove.com>
2023-03-17 14:37:25 +0200

    all tasks routes added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,26 @@ const getAllTasks = (req, res) => {
     res.send('all items')
 }
 
+const createTask = (req, res) => {
+    res.send('create task')
+}
+
+const getTask = (req, res) => {
+    res.send('get a single item')
+}
+
+const updateTask = (req, res) => {
+    res.send('update a task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('delete a task')
+}
+
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 }
