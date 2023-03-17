e14c475fbf82d5eeec10d07728bd43da16bc4800
Koray Dundar <koray.dundar@exove.com>
2023-03-17 14:27:09 +0200

    Get all task

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
 
+// middleware
+app.use(express.json())
+
+// routes
 app.get('/hello', (req, res) => {
     res.send('Hello world!')
 })
+
+app.use('/api/v1/task', tasks)
 
 const PORT = 3000
 
