dd6149254368ef4b5d5e871578af0427c1c038b8
Koray Dundar <koray.dundar@exove.com>
2023-03-21 16:34:22 +0200

    delete task is added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -37,8 +37,19 @@ const updateTask = (req, res) => {
     res.send('update a task')
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete a task')
+const deleteTask = async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id: req.params.id })
+
+        if(!task) {
+            return res.status(404).json({msg:`No task with id: ${req.params.id}`})
+        }
+
+        res.status(200).json({ task: task })
+
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
 }
 
 module.exports = {
