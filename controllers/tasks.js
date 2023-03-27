0e11862719ff716746585eff2efb37510050c364
Koray Dundar <koray.dundar@exove.com>
2023-03-27 11:32:20 +0300

    Fixed error

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,8 +2,8 @@ const Task = require('../models/Task')
 
 const getAllTasks = async (req, res) => {
     try {
-        const task = await Task.find({})
-        res.status(200).json({ task: task })
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
     } catch (error) {
         res.status(500).json({ msg: error })
     }
@@ -12,7 +12,7 @@ const getAllTasks = async (req, res) => {
 const createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body)
-        res.status(201).json({ task: task})
+        res.status(201).json({ task })
     } catch (error) {
         res.status(500).json({ msg: error })
     }
